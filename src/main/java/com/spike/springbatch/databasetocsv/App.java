20a4bd9eaf980777b1756a73b6e3df5fb86c21cb
cuipengfei <cuipengfei2008@gmail.com>
2014-01-07 01:08:48 +0800

    clean up app

--- src/main/java/com/spike/springbatch/databasetocsv/App.java
+++ src/main/java/com/spike/springbatch/databasetocsv/App.java
@@ -1,13 +1,13 @@
 package com.spike.springbatch.databasetocsv;
 
 import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
+
     public static void main(String[] args) {
 
         String[] springConfig =
@@ -16,22 +16,15 @@ public class App {
                         "spring/batch/jobs/person-table-export-job.xml"
                 };
 
-        ApplicationContext context =
-                new ClassPathXmlApplicationContext(springConfig);
+        ApplicationContext context = new ClassPathXmlApplicationContext(springConfig);
 
         JobLauncher jobLauncher = (JobLauncher) context.getBean("jobLauncher");
         Job job = (Job) context.getBean("personTableExportJob");
 
         try {
-
-            JobExecution execution = jobLauncher.run(job, new JobParameters());
-            System.out.println("Exit Status : " + execution.getStatus());
-
+            jobLauncher.run(job, new JobParameters());
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-        System.out.println("Done");
-
     }
 }
